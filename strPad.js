005142a06c08effe96b8d34ed4c6fff6249c5554
Arttu Manninen <arttu@kaktus.cc>
2020-02-22 21:54:20 +0200

    Added string padding or strPad function

---
+++ strPad.js
@@ -0,0 +1,32 @@
+/**
+ * String padding
+ *
+ * @param { string } input          String to pad
+ * @param { string } fill           Fill-in string
+ * @param { number } length         String length
+ * @param { boolean } [left         Pad to left on true, right on false
+ * @return { string}                Localized string
+ */
+module.exports = function strPad (input, fill = ' ', length = 0, left = true) {
+  if (!fill) {
+    fill = ' '
+  }
+
+  if (length <= 0) {
+    return input
+  }
+
+  while (input.length < length) {
+    if (left) {
+      input = `${fill}${input}`
+    } else {
+      input += fill
+    }
+  }
+
+  if (left) {
+    return input.substr(input.length - length)
+  }
+
+  return input.substr(0, length)
+}
---
+++ tests/strPad.js
@@ -0,0 +1,27 @@
+const expect = require('expect.js')
+const strPad = require('../strPad')
+
+describe('strPad', () => {
+  it('should pad string to the given length', (done) => {
+    expect(strPad('foo', ' ', 5, true)).to.be('  foo')
+    expect(strPad('foo', ' ', 5, false)).to.be('foo  ')
+    done()
+  })
+
+  it('should force an empty filling for zero length fill', (done) => {
+    expect(strPad('foo', '', 5, true)).to.be('  foo')
+    expect(strPad('foo', '', 5, false)).to.be('foo  ')
+    done()
+  })
+
+  it('should leave the input intact for zero length', (done) => {
+    expect(strPad('foo', '', 0)).to.be('foo')
+    done()
+  })
+
+  it('should trim the output when padding exceeds the length', (done) => {
+    expect(strPad('foo', '1234567', 4, true)).to.be('7foo')
+    expect(strPad('foo', '1234567', 4, false)).to.be('foo1')
+    done()
+  })
+})
