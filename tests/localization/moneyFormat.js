18e20382761a780238277b8d5771a3f2d5d6142e
Arttu Manninen <arttu@kaktus.cc>
2020-02-25 00:08:26 +0200

    Added money format to localization

---
+++ tests/localization/moneyFormat.js
@@ -0,0 +1,37 @@
+const expect = require('expect.js')
+const Localization = require('../../lib/localization')
+
+describe('Localization moneyFormat', () => {
+  it('should output with default locale', (done) => {
+    const l10n = new Localization()
+    l10n.unregisterLocales('moneyFormat')
+
+    expect(l10n.moneyFormat(10, true)).to.be('10.00 e')
+    expect(l10n.moneyFormat(10, false)).to.be('10 e')
+    done()
+  })
+
+  it('should output with custom locale', (done) => {
+    const l10n = new Localization()
+    l10n.registerLocales({
+      moneyFormat: {
+        en: '$%s',
+        fi: '€%s'
+      },
+      decimalSeparator: {
+        en: 'x',
+        fi: 'X'
+      }
+    })
+
+    l10n.setLang('en')
+    expect(l10n.moneyFormat(10)).to.be('$10x00')
+
+    l10n.setLang('fi')
+    expect(l10n.moneyFormat(10)).to.be('€10X00')
+
+    l10n.unregisterLocales('moneyFormat')
+    l10n.unregisterLocales('decimalSeparator')
+    done()
+  })
+})
