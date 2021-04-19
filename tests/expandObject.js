1d625d136306a7ccc96a1762dafb9d4ed18445b0
Arttu Manninen <arttu@kaktus.cc>
2021-04-19 16:21:53 +0300

    Added new methods flattenObject and expandObject

---
+++ tests/expandObject.js
@@ -0,0 +1,39 @@
+const expect = require('expect.js')
+const expandObject = require('../lib/expandObject')
+const flattenObject = require('../lib/flattenObject')
+
+describe('lib/expandObject', () => {
+  it('should return non-objects as they were', (done) => {
+    expect(expandObject(null)).to.eql(null)
+    expect(expandObject(undefined)).to.eql(undefined)
+    expect(expandObject(1.23)).to.eql(1.23)
+    expect(expandObject('foo')).to.eql('foo')
+    expect(expandObject(['foo', 'bar'])).to.eql(['foo', 'bar'])
+    done()
+  })
+
+  it('should expand an object', (done) => {
+    const source = {
+      'foo.bar': 'foo',
+      'foo.array': ['foo', 'bar'],
+      'foo.nested.deep.object': true
+    }
+
+    const value = expandObject(source)
+    expect(value).to.eql({
+      foo: {
+        bar: 'foo',
+        array: [
+          'foo',
+          'bar'
+        ],
+        nested: {
+          deep: {
+            object: true
+          }
+        }
+      }
+    })
+    done()
+  })
+})
---
+++ tests/flattenObject.js
@@ -0,0 +1,36 @@
+const expect = require('expect.js')
+const flattenObject = require('../lib/flattenObject')
+
+describe('lib/flattenObject', () => {
+  it('should return non-objects as they were', (done) => {
+    expect(flattenObject(null)).to.eql(null)
+    expect(flattenObject(undefined)).to.eql(undefined)
+    expect(flattenObject(1.23)).to.eql(1.23)
+    expect(flattenObject('foo')).to.eql('foo')
+    expect(flattenObject(['foo', 'bar'])).to.eql(['foo', 'bar'])
+    done()
+  })
+
+  it('should flatten a deep object', (done) => {
+    const source = {
+      foo: {
+        bar: 'foo',
+        array: [
+          'foo',
+          'bar'
+        ],
+        nested: {
+          deep: {
+            object: true
+          }
+        }
+      }
+    }
+
+    const value = flattenObject(source)
+    expect(value['foo.bar']).to.eql(source.foo.bar)
+    expect(value['foo.array']).to.eql(source.foo.array)
+    expect(value['foo.nested.deep.object']).to.eql(true)
+    done()
+  })
+})
