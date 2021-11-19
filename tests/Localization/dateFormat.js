247635c097a1511b83b5353558e252160ef557c0
Arttu Manninen <arttu@kaktus.cc>
2021-11-19 13:14:49 +0200

    Added dateFormat and timeFormat functions to Localization

---
+++ tests/Localization/dateFormat.js
@@ -0,0 +1,39 @@
+const expect = require('expect.js')
+const Localization = require('../../lib/Localization')
+
+describe('lib/Localization dateFormat', () => {
+  it('should output with default locale', (done) => {
+    const l10n = new Localization()
+    l10n.unregisterLocales('dateFormat')
+
+    expect(l10n.dateFormat('2021-11-19T12:00:00')).to.be('2021-11-19')
+    done()
+  })
+
+  it('should output with custom locale', (done) => {
+    const l10n = new Localization()
+    l10n.registerLocales({
+      dateFormat: {
+        en: 'MM/DD/YYYY',
+        fi: 'DD-MM-YYYY'
+      }
+    })
+
+    l10n.setLang('en')
+    expect(l10n.dateFormat('2021-11-19T12:00:00')).to.be('11/19/2021')
+
+    l10n.setLang('fi')
+    expect(l10n.dateFormat('2021-11-19T12:00:00')).to.be('19-11-2021')
+
+    l10n.unregisterLocales('dateFormat')
+    done()
+  })
+
+  it('should throw INVALID_TIMESTAMP error for an invalid timestamp', (done) => {
+    const l10n = new Localization()
+    expect(l10n.dateFormat).withArgs('abcdef').to.throwException((err) => {
+      expect(err).to.be.a(Localization.errors.INVALID_TIMESTAMP)
+    })
+    done()
+  })
+})
---
+++ tests/Localization/timeFormat.js
@@ -0,0 +1,39 @@
+const expect = require('expect.js')
+const Localization = require('../../lib/Localization')
+
+describe('lib/Localization timeFormat', () => {
+  it('should output with default locale', (done) => {
+    const l10n = new Localization()
+    l10n.unregisterLocales('timeFormat')
+
+    expect(l10n.timeFormat('12:00:00')).to.be('12:00:00')
+    done()
+  })
+
+  it('should output with custom locale', (done) => {
+    const l10n = new Localization()
+    l10n.registerLocales({
+      timeFormat: {
+        en: 'HH:mm',
+        fi: 'HH.mm'
+      }
+    })
+
+    l10n.setLang('en')
+    expect(l10n.timeFormat('2021-11-19T12:00:00')).to.be('12:00')
+
+    l10n.setLang('fi')
+    expect(l10n.timeFormat('2021-11-19T12:00:00')).to.be('12.00')
+
+    l10n.unregisterLocales('timeFormat')
+    done()
+  })
+
+  it('should throw INVALID_TIMESTAMP error for an invalid timestamp', (done) => {
+    const l10n = new Localization()
+    expect(l10n.timeFormat).withArgs('abcdef').to.throwException((err) => {
+      expect(err).to.be.a(Localization.errors.INVALID_TIMESTAMP)
+    })
+    done()
+  })
+})
