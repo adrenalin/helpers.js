caeb55fa4abf24cf0fae4f9f236a2420486a9e82
Arttu Manninen <arttu@kaktus.cc>
2023-01-18 16:02:33 +0200

    New method getRoundedNumber

--- test/Localization/basics.js
+++ test/Localization/basics.js
@@ -1,18 +1,14 @@
 const { expect } = require('chai')
+const { InvalidArgument } = require('@vapaaradikaali/errors')
 const Localization = require('../../lib/Localization')
 
 describe('lib/Localization basics', () => {
-  it('should be a class that can be initialized', (done) => {
-    try {
-      const l10n = new Localization()
-      expect(l10n).to.be.an.instanceof(Localization)
-      done()
-    } catch (err) {
-      done(err)
-    }
+  it('should be a class that can be initialized', () => {
+    const l10n = new Localization()
+    expect(l10n).to.be.an.instanceof(Localization)
   })
 
-  it('should have both static and instance errors', (done) => {
+  it('should have both static and instance errors', () => {
     const l10n = new Localization()
 
     expect(l10n.errors).to.be.an('object')
@@ -20,23 +16,20 @@ describe('lib/Localization basics', () => {
     expect(Localization.errors).to.equal(l10n.errors)
 
     expect(Localization.INVALID_TIMESTAMP).to.equal(Localization.errors.INVALID_TIMESTAMP)
-    done()
   })
 
-  it('should accept a language as an argument', (done) => {
+  it('should accept a language as an argument', () => {
     const lang = 'fi'
     const l10n = new Localization(lang)
     expect(l10n.lang).to.equal(lang)
-    done()
   })
 
-  it('should have a shorthand in a class instance', (done) => {
+  it('should have a shorthand in a class instance', () => {
     const l10n = new Localization()
     expect(l10n.registerLocales).to.equal(Localization.registerLocales)
-    done()
   })
 
-  it('should register locales', (done) => {
+  it('should register locales', () => {
     const locales = {
       foo: {
         en: 'Foo',
@@ -46,10 +39,9 @@ describe('lib/Localization basics', () => {
 
     const l10n = new Localization()
     l10n.registerLocales(locales)
-    done()
   })
 
-  it('should have registerLocale', (done) => {
+  it('should have registerLocale', () => {
     const locales = {
       registerLocale: {
         en: 'Register locale via instance method'
@@ -64,56 +56,42 @@ describe('lib/Localization basics', () => {
 
     expect(l10n.getInLang('en', 'registerLocale')).to.eql(locales.registerLocale.en)
     expect(l10n.getInLang('en', 'registerLocaleStatic')).to.eql(locales.registerLocaleStatic.en)
-    done()
   })
 
-  it('should be able to set the language', (done) => {
+  it('should be able to set the language', () => {
     const lang = 'fi'
     const l10n = new Localization()
     const rval = l10n.setLang(lang)
     expect(l10n.lang).to.equal(lang)
     expect(rval).to.equal(l10n)
-    done()
   })
 
-  it('should reject an empty string when setting the language', (done) => {
-    try {
-      const l10n = new Localization()
-      l10n.setLang('')
-      done(new Error('Should have thrown an error'))
-    } catch (err) {
-      done()
-    }
+  it('should reject an empty string when setting the language', () => {
+    const l10n = new Localization()
+    expect(() => l10n.setLang('')).to.throw()
   })
 
-  it('should require a string when setting the language', (done) => {
-    try {
-      const l10n = new Localization()
-      l10n.setLang({})
-      done(new Error('Should have thrown an error'))
-    } catch (err) {
-      done()
-    }
+  it('should require a string when setting the language', () => {
+    const l10n = new Localization()
+    expect(() => l10n.setLang({})).to.throw()
   })
 
-  it('should be able to get the set language', (done) => {
+  it('should be able to get the set language', () => {
     const lang = 'fi'
     const l10n = new Localization()
     l10n.setLang(lang)
     expect(l10n.getLang()).to.equal(lang)
-    done()
   })
 
-  it('should reject to register invalid locales', (done) => {
+  it('should reject to register invalid locales', () => {
     const l10n = new Localization()
     expect(() => l10n.registerLocales('foo')).to.throw()
     expect(() => l10n.registerLocales({ foo: 'bar' })).to.throw()
     expect(() => l10n.registerLocales({ foo: [] })).to.throw()
     expect(() => l10n.registerLocales({ foo: { bar: [] } })).to.throw()
-    done()
   })
 
-  it('should be able to use fallback language', (done) => {
+  it('should be able to use fallback language', () => {
     const locales = {
       foo: {
         en: 'Foo'
@@ -125,30 +103,19 @@ describe('lib/Localization basics', () => {
     l10n.registerLocales(locales)
     expect(l10n.getInLang('fi', 'foo')).to.equal(locales.foo.en)
     expect(rval).to.equal(l10n)
-    done()
   })
 
-  it('should reject an empty string when setting the fallback language', (done) => {
-    try {
-      const l10n = new Localization()
-      l10n.setFallbackLang('')
-      done(new Error('Should have thrown an error'))
-    } catch (err) {
-      done()
-    }
+  it('should reject an empty string when setting the fallback language', () => {
+    const l10n = new Localization()
+    expect(() => l10n.setFallbackLang(''))
   })
 
-  it('should require a string when setting the fallback language', (done) => {
-    try {
-      const l10n = new Localization()
-      l10n.setFallbackLang({})
-      done(new Error('Should have thrown an error'))
-    } catch (err) {
-      done()
-    }
+  it('should require a string when setting the fallback language', () => {
+    const l10n = new Localization()
+    expect(() => l10n.setFallbackLang({}))
   })
 
-  it('should spread an array', (done) => {
+  it('should spread an array', () => {
     const l10n = new Localization()
     l10n.registerLocales({
       testSpreadAnArray: {
@@ -157,6 +124,14 @@ describe('lib/Localization basics', () => {
     })
 
     expect(l10n.getInLang('fi', ['testSpreadAnArray', 1, 2, 3])).to.eql('1 2 3')
-    done()
+  })
+
+  it('should get valid rounded numbers', () => {
+    expect(() => Localization.getRoundedNumber('foo')).to.throw(InvalidArgument)
+
+    expect(Localization.getRoundedNumber(1.234)).to.eql(1.234, 'No precision given, do not round')
+    expect(Localization.getRoundedNumber(1.234, 1, 'ceil')).to.eql(1.3, 'Ceil to one decimal')
+    expect(Localization.getRoundedNumber(1.28, 1, 'floor')).to.eql(1.2, 'Floor to one decimal')
+    expect(Localization.getRoundedNumber(123, -1)).to.eql(120, 'Round to closest tens')
   })
 })
