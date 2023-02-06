a77f4fdd4e16c98a0443bb861f8f71bcaf016d66
Arttu Manninen <arttu@kaktus.cc>
2023-02-06 17:17:22 +0200

    Accept number as the first argument, added type checking for the arguments

--- test/strPad.js
+++ test/strPad.js
@@ -1,27 +1,42 @@
+const { InvalidArgument } = require('@vapaaradikaali/errors')
 const { expect } = require('chai')
 const strPad = require('../lib/strPad')
 
 describe('lib/strPad', () => {
-  it('should pad string to the given length', (done) => {
+  it('should pad string to the given length', () => {
     expect(strPad('foo', ' ', 5, true)).to.equal('  foo')
     expect(strPad('foo', ' ', 5, false)).to.equal('foo  ')
-    done()
   })
 
-  it('should force an empty filling for zero length fill', (done) => {
+  it('should force an empty filling for zero length fill', () => {
     expect(strPad('foo', '', 5, true)).to.equal('  foo')
     expect(strPad('foo', '', 5, false)).to.equal('foo  ')
-    done()
   })
 
-  it('should leave the input intact for zero length', (done) => {
-    expect(strPad('foo', '', 0)).to.equal('foo')
-    done()
-  })
-
-  it('should trim the output when padding exceeds the length', (done) => {
+  it('should trim the output when padding exceeds the length', () => {
     expect(strPad('foo', '1234567', 4, true)).to.equal('7foo')
     expect(strPad('foo', '1234567', 4, false)).to.equal('foo1')
-    done()
+  })
+
+  it('should accept strings, numbers, null and undefined as the first argument', () => {
+    expect(strPad(1, '0', 2)).to.eql('01')
+    expect(strPad('1', '0', 2)).to.eql('01')
+    expect(strPad(null, '0', 2)).to.eql('00')
+    expect(() => strPad([], '0', 2)).to.throw(InvalidArgument)
+  })
+
+  it('should accept strings, numbers, null and undefined as the second argument', () => {
+    expect(strPad('1', '0', 2)).to.eql('01')
+    expect(strPad('1', 0, 2)).to.eql('01')
+    expect(strPad('1', null, 2)).to.eql(' 1')
+    expect(() => strPad('0', [], 2)).to.throw(InvalidArgument)
+  })
+
+  it('should accept a positive integer or a similar string as the third argument', () => {
+    expect(() => strPad(1, 0, 2)).not.to.throw()
+    expect(() => strPad(1, 0, '2')).not.to.throw()
+
+    expect(() => strPad(1, 0, -2)).to.throw(InvalidArgument)
+    expect(() => strPad(1, 0, 1.2)).to.throw(InvalidArgument)
   })
 })
