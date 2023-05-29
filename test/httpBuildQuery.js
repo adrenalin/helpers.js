cd42c4fe60d031b7cb4a848389e2fdf9bda0dfec
Arttu Manninen <arttu@kaktus.cc>
2023-05-29 06:24:21 +0300

    Options for modifyUrl and httpBuildQuery

--- test/httpBuildQuery.js
+++ test/httpBuildQuery.js
@@ -2,7 +2,7 @@ const { expect } = require('chai')
 const { httpBuildQuery } = require('../')
 
 describe('lib/httpBuildQuery', () => {
-  it('should accept only objects', (done) => {
+  it('should accept only objects', () => {
     expect(() => httpBuildQuery(1)).to.throw()
     expect(() => httpBuildQuery('1')).to.throw()
     expect(() => httpBuildQuery([])).to.throw()
@@ -10,44 +10,40 @@ describe('lib/httpBuildQuery', () => {
     expect(() => httpBuildQuery(true)).to.throw()
 
     expect(() => httpBuildQuery({})).not.to.throw()
-    done()
   })
 
-  it('should convert a basic object to a query string', (done) => {
+  it('should convert a basic object to a query string', () => {
     expect(httpBuildQuery({ foo: 'bar', bar: 'foo', foobar: 1 })).to.equal('foo=bar&bar=foo&foobar=1')
-    done()
   })
 
-  it('should convert a deep object to a query string', (done) => {
+  it('should convert a deep object to a query string', () => {
     expect(httpBuildQuery({ foo: { bar: 'foo', foobar: 1 } })).to.equal('foo[bar]=foo&foo[foobar]=1')
-    done()
   })
 
-  it('should skip null values', (done) => {
+  it('should skip null values', () => {
     expect(httpBuildQuery({ foo: null })).to.equal('')
     expect(httpBuildQuery({ foo: undefined })).to.equal('')
-    done()
   })
 
-  it('should convert boolean values', (done) => {
+  it('should convert boolean values', () => {
     expect(httpBuildQuery({ foo: true })).to.equal('foo=true')
     expect(httpBuildQuery({ foo: false })).to.equal('foo=false')
-    done()
   })
 
-  it('should convert array values', (done) => {
+  it('should convert array values', () => {
     expect(httpBuildQuery({ foo: [1, 2, 4, 6] })).to.equal('foo[0]=1&foo[1]=2&foo[2]=4&foo[3]=6')
-    done()
   })
 
-  it('should convert array values recursively', (done) => {
+  it('should convert array values recursively', () => {
     expect(httpBuildQuery({ foo: [[1], [2], [4], [6]] })).to.equal('foo[0][0]=1&foo[1][0]=2&foo[2][0]=4&foo[3][0]=6')
     expect(httpBuildQuery({ foo: [{ foo: 'bar', bar: null }] })).to.equal('foo[0][foo]=bar')
-    done()
   })
 
-  it('should keep null values in the array', (done) => {
+  it('should keep null values in the array', () => {
     expect(httpBuildQuery({ foo: [1, null, 4, 6] })).to.equal('foo[0]=1&foo[1]=&foo[2]=4&foo[3]=6')
-    done()
+  })
+
+  it('should not set array keys when requested not to', () => {
+    expect(httpBuildQuery({ foo: [1, null, 4, 6] }, { arrayKeys: false })).to.equal('foo[]=1&foo[]=&foo[]=4&foo[]=6')
   })
 })
--- test/modifyUrl.js
+++ test/modifyUrl.js
@@ -2,29 +2,29 @@ const { expect } = require('chai')
 const { modifyUrl } = require('../')
 
 describe('lib/modifyUrl', () => {
-  it('should accept only a string for url', (done) => {
+  it('should accept only a string for url', () => {
     expect(() => modifyUrl(0)).to.throw()
     expect(() => modifyUrl([0])).to.throw()
     expect(() => modifyUrl({ foo: 'bar' })).to.throw()
     expect(() => modifyUrl(null)).to.throw()
-
-    done()
   })
 
-  it('should return url without query string with an empty object', (done) => {
+  it('should return url without query string with an empty object', () => {
     expect(modifyUrl('/foo')).to.equal('/foo')
     expect(modifyUrl('/foo', {})).to.equal('/foo')
-    done()
   })
 
-  it('should append query string to a URL', (done) => {
+  it('should append query string to a URL', () => {
     expect(modifyUrl('/foo', { foo: 'bar' })).to.equal('/foo?foo=bar')
     expect(modifyUrl('/foo?bar=foo', { foo: 'bar' })).to.equal('/foo?bar=foo&foo=bar')
-    done()
   })
 
-  it('should replace existing query values with new', (done) => {
+  it('should replace existing query values with new', () => {
     expect(modifyUrl('/foo?foo=foo', { foo: 'bar' })).to.equal('/foo?foo=bar')
-    done()
+  })
+
+  it('should pass the optional options', () => {
+    expect(modifyUrl('/foo?foo[]=foo', { foo: ['bar'] })).to.equal('/foo?foo[0]=bar')
+    expect(modifyUrl('/foo?foo[]=foo', { foo: ['bar'] }, { arrayKeys: false })).to.equal('/foo?foo[]=bar')
   })
 })
