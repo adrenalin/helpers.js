3bdf722a86ceb055a22667fb752872830f5e6ee3
Arttu Manninen <arttu@kaktus.cc>
2023-08-18 16:11:28 +0300

    New function getObjectPaths

---
+++ test/getObjectPaths.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai')
+const { getPath, getObjectPaths } = require('../')
+
+describe('lib/getObjectPaths', () => {
+  it('should return object paths for a flat object', () => {
+    expect(getObjectPaths({ foo: 'bar', bar: 'foo' })).to.eql(['foo', 'bar'])
+  })
+
+  it('should return paths of a deep object', () => {
+    expect(getObjectPaths({ foo: { bar: 'foobar' }})).to.eql(['foo', 'foo.bar'])
+  })
+
+  it('should return array with indexes', () => {
+    expect(getObjectPaths({ foo: ['foo', { bar: 'foobar' }]})).to.eql([
+      'foo',
+      'foo.0',
+      'foo.1',
+      'foo.1.bar'
+    ])
+  })
+})
